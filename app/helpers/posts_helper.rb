40547890293debc95f21fcf66bd3c91f18d851e2
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-02-08 22:09:37 -0500

    service_img_links_started

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,13 @@
 module PostsHelper
+  def post_img tag
+    post = Post.find_by_tag tag
+    if post and post.image.present?
+      post.image
+    else
+      nil
+    end
+  end
+  
   def post_text tag
     post = Post.find_by_tag(tag)
     if post and post.body.present?
