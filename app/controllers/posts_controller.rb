2267cb1d87669980c1485fe9cf6d54f8127504da
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-06-29 19:41:06 -0400

    links_and_secure_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:update]
+  before_action :secure_user
   
   def new
     @post = Post.new
@@ -46,36 +47,13 @@ class PostsController < ApplicationController
     redirect_to :back
   end
   
-  def carousel_scroll
-    @carousel_size = 4
-    @direction = params[:direction]
-    # steps up and down carousel_index
-    cookies[:carousel_index] = if @direction.to_sym.eql? :right
-      cookies[:carousel_index] = cookies[:carousel_index].to_i + 1
-    elsif @direction.to_sym.eql? :left
-      cookies[:carousel_index] = cookies[:carousel_index].to_i - 1
-    end
-    # keeps within limits
-    if cookies[:carousel_index].to_i > Post.all.size - @carousel_size
-      cookies[:carousel_index] = 0
-    elsif cookies[:carousel_index].to_i < 0
-      cookies[:carousel_index] = Post.all.size - @carousel_size
-    end
-    # gets images
-    @images = Post.limit(@carousel_size).offset(cookies[:carousel_index].to_i)
-    # carousel image to insert with ids
-    if @direction == "right"
-      @image = @images.last
-      @point_to_add = "last"
-      @point_to_remove ="first"
-    elsif @direction == "left"
-      @image = @images.first
-      @point_to_add = "first"
-      @point_to_remove = "last"
+  private
+  
+  def secure_user
+    unless current_user
+      redirect_to '/404'
     end
   end
-  
-  private
   
   def set_post
     @post = Post.find params[:id]
